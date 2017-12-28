1aa2bce5d5ffa0a9c103d5ee5154a916c9b1241e
gayan madurapperuma <madurapperumamags@gmail.com>
2017-12-29 01:06:29 +0530

    intial commit

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname+'/public'));
+
+//Implementing middleware function
+app.use((req,res,next) => {
+    var now = new Date().toString();
+    var log = `${now} : ${req.method}, ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log',log + '\n',(err) => {
+        if (err)
+            console.log('Unable to append file server.log');
+    });
+    next();
+});
+
+app.use((req,res,next) => {
+    res.render('maintenance.hbs');
+})
+
+//HANDLEBAR FUNCTION 
+hbs.registerHelper('getCurrentYear',() => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('functionWithArgument',(text) => {
+    // console.log(text);
+    // var ts = text;
+    return text.toUpperCase();
+})
+
+app.get('/',(req,res) => {
+    //res.send('Hello Express');
+    // res.send({
+    //     name : 'gayan',
+    //     likes : [
+    //         'pakaya','hutta'
+    //     ]
+    // })
+    res.render('home.hbs',{
+       pageT : 'Home',
+       pageTitle : 'Home page',
+       welcomeMessage : 'Welcome home',
+       currentYear : new Date().getFullYear() 
+    })
+});
+
+//Routing Test
+app.get('/about',(req,res) => {
+    //res.render('about.hbs');
+    // res.send('This is fucking about page!');
+    res.render('about.hbs',{
+        pageTitle : 'fucking page',
+        currentYear : new Date().getFullYear()
+    });
+});
+
+app.listen(3000);
